53081b7e40bbc014e8ea171640b01daddc18869b
faraway10 <42tree42@gmail.com>
2024-06-21 23:20:05 +0300

    Flat json files comparing implemented; README.md updated

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/faraway10/java-project-71/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/faraway10/java-project-71/actions)
+
+### Diff checker
+#### The project offers you ability to compare two flat json files
+
+##### Two files with json:
+![file1.json and file2.json](https://sun1-91.userapi.com/impg/PZOTMfzrn2HApnzkAPQdZDi7nlsv_QodWW4Fwg/vN0HGyX8OAE.jpg?size=745x252&quality=95&sign=f94b8c58c9c94ec1c3a72f5a10badc05&type=album "file1.json and file2.json")
+##### Result of comparing:
+![Even: win](https://sun9-67.userapi.com/impg/qZ7VBrD-7eocvxbwyaNbp_jozP4wy_88La7MZg/Shb9T8LNww0.jpg?size=581x300&quality=95&sign=32a1576637b4e542da0f4cfdfe007dd3&type=album "Even: win")
--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -3,13 +3,20 @@ package hexlet.code;
 import picocli.CommandLine;
 import picocli.CommandLine.Option;
 import picocli.CommandLine.Parameters;
+
+import java.util.*;
 import java.util.concurrent.Callable;
+
+import java.nio.file.Paths;
+import java.nio.file.Files;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.core.type.TypeReference;
 
 @CommandLine.Command(name = "gendiff", mixinStandardHelpOptions = true, version = "gendiff 1.0",
         description = "Compares two configuration files and shows a difference.")
 
 public class App implements Callable<Integer> {
-
     @Parameters(index = "0", paramLabel = "filepath1", description = "path to first file")
     private String filepath1;
 
@@ -32,6 +39,42 @@ public class App implements Callable<Integer> {
 
     @Override
     public Integer call() throws Exception {
+        String jsonStr1 = Files.readString(Paths.get(filepath1));
+        String jsonStr2 = Files.readString(Paths.get(filepath2));
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        Map<String, String> map1 = objectMapper.readValue(jsonStr1, new TypeReference<Map<String, String>>(){});
+        Map<String, String> map2 = objectMapper.readValue(jsonStr2, new TypeReference<Map<String, String>>(){});
+
+        List<String> allKeys = new ArrayList<String>();
+
+        allKeys.addAll(map1.keySet());
+        allKeys.addAll(map2.keySet());
+
+        allKeys = allKeys.stream().distinct().sorted().toList();
+        StringJoiner result = new StringJoiner("\n", "{\n", "\n}");
+
+        for (String key:allKeys) {
+            boolean isEqual = Objects.equals(map1.get(key), map2.get(key));
+            boolean isRemoved = map1.containsKey(key) && !map2.containsKey(key);
+            boolean isAdded = !map1.containsKey(key) && map2.containsKey(key);
+            boolean isChanged = !isEqual && !isRemoved && !isAdded;
+
+            if (isEqual) {
+                result.add("    " + key + ": " + map1.get(key));
+            }
+
+            if (isRemoved || isChanged) {
+                result.add("  - " + key + ": " + map1.get(key));
+            }
+
+            if (isAdded || isChanged) {
+                result.add("  + " + key + ": " + map2.get(key));
+            }
+        }
+
+        System.out.println(result.toString());
+
         return 0;
     }
 }
