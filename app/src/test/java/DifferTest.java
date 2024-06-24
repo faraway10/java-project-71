05de3d25f76236e663b2fb650981ea5fc0590038
faraway10 <42tree42@gmail.com>
2024-06-24 21:29:36 +0300

    wrong test fixed

--- app/src/test/java/DifferTest.java
+++ app/src/test/java/DifferTest.java
@@ -10,7 +10,7 @@ public class DifferTest {
                 + "    host: hexlet.io\n"
                 + "  - proxy: 123.234.53.22\n"
                 + "  - timeout: 50\n"
-                + "  + timeout: 200\n"
+                + "  + timeout: 20\n"
                 + "  + verbose: true\n"
                 + "}";
         String actual = Differ.generate("src/test/resources/file1.json", "src/test/resources/file2.json");
